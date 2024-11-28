2a881ce219ff7c52cf2578d97ec6350e05a5fa62
Stefanospedicato <stefano.spedicato11@gmail.com>
2024-11-28 14:56:21 +0100

    feat: add functions in script.js

--- script.js
+++ script.js
@@ -36,3 +36,33 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+
+function creazioneCarta(member){
+  const {name , role , email ,img} = member;
+  const carta = `
+  <div class="col-3 mt-5 offset-1 d-flex">
+      <div id="image-box">
+          <img class="" src="${img}" alt="{${name}}">
+      </div>
+      <div class="flex-grow" id="description">
+          <h3 class="mt-5 m-3">${name}</h3>
+          <h6 class="m-3">${role}</h6>
+          <a class="m-3" href="#">${email}</a>
+      </div>
+  </div>
+  `
+  return carta;
+}
+
+const row = document.querySelector('.row');
+
+function myTeam(array) {
+  let carte = '';
+  for(let member of teamMembers){
+    carte += creazioneCarta(member);
+  }
+  row.innerHTML = carte
+}
+
+myTeam(teamMembers);
